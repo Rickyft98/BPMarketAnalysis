7163834bfa518fe42e15ad88b98362c6b23cb903
Ricky Fuenmayor <rickyft98@gmail.com>
2025-11-20 15:49:19 +0000

    V1.2

--- ScrapAllpages.py
+++ ScrapAllpages.py
@@ -2,47 +2,63 @@ import requests
 import pandas as pd
 import json
 
-def get_all_recipes(main_category=""):
+recipes_id=pd.read_csv('config/all_recipes.csv')
+idrow=recipes_id['id'].tolist() #Database id collums from all recipes
+AllYieldData = []
+
+def get_all_recipes():
     all_recipes = []
-    page = 1
-    
-    while True:
+   
+    for i in enumerate(idrow):
         params = {
-            "language": "en",
-            "page": page,
-            "mainCategory": main_category,
-            "subCategory": "",
-            "facets": {}
+            "id": str(idrow[i]),
+            "language": "en"
         }
-        
-        base_url = 'https://questlog.gg/blue-protocol/api/trpc/database.getRecipes'
+
+    
+        # Make request
+        base_url = 'https://questlog.gg/blue-protocol/api/trpc/database.getRecipe'
         r = requests.get(base_url, params={'input': json.dumps(params)})
         data = r.json()
         
-        recipes = data['result']['data']['pageData']
-        
-        if not recipes:  # No more recipes
-            break
-            
-        all_recipes.extend(recipes)
-        print(f"Page {page}: {len(recipes)} recipes")
-        
-        # Check if we've reached the last page
-        current_page = data['result']['data']['currentPage']
-        total_pages = data['result']['data']['pageCount']
-        
-        if current_page >= total_pages:
-            break
-            
-        page += 1
-    
-    return all_recipes
 
+
+        # Process data
+        Recipesinfo = data['result']['data']['recipeInputItems']
+        input_items_flat = []
+        for item_group in Recipesinfo:
+            for item_data in item_group:
+                input_items_flat.append({
+                    'input_id': item_data['item']['id'],
+                    'item_name': item_data['item']['name'],
+                    'amount': item_data['amount'],
+                    'isVariable': item_data['isVariable'],
+                    'grade': item_data['item']['grade'],
+                    'mainCategory': item_data['item']['mainCategory'],
+                    'subCategory': item_data['item']['subCategory']
+                })
+
+        recipe_data = data['result']['data']  
+        final_input = [{
+            'id': recipe_data['id'],
+            'name': recipe_data['name'],
+            'icon': recipe_data['icon'],
+            'grade': recipe_data['grade'],
+            'dbType': recipe_data['dbType'],
+            'mainCategory': recipe_data['mainCategory'],
+            'description': recipe_data['description'],
+            'FocusCost': recipe_data['cost']['amount'],
+            'input_data': input_items_flat
+        }]
+        all_recipes.extend(final_input)
+
+'''
 # Get ALL recipes
 print("Downloading all recipes...")
-all_recipes = get_all_recipes("culinary")
+all_recipes = get_all_recipes("")
 df = pd.DataFrame(all_recipes)
 
 print(f"Total recipes: {len(df)}")
-df.to_csv('all_recipes.csv', index=False)
-print("Saved all recipes to all_recipes.csv")
+df.to_csv('all_collectable.csv', index=False)
+print("Saved all recipes to all_collectable.csv")
+'''
